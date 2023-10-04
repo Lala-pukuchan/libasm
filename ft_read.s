f53b8afde07baeafbe6552a1334277043e4c6c70
Lala-pukuchan <ruruover1105@gmail.com>
2023-10-04 18:04:06 +0900

    global error

--- ft_read.s
+++ ft_read.s
@@ -1,19 +1,8 @@
 global ft_read
-extern __errno_location
 
 section .text
 ft_read:
     mov rax, 0
     syscall
 	test rax, rax
-	js .handle_error
     ret
-
-.handle_error:
-    mov rdi, rax
-	call __errno_location
-	neg rdi
-	mov [rax], rdi
-
-	mov rax, -1
-	ret
